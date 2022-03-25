178832f8aa77a1cd1330df8f6d68d3bac91e8d8c
gulshan25 <g.r.tanny@gmail.com>
2022-03-26 02:10:48 +0600

    codecademy

---
+++ script.js
@@ -0,0 +1,15 @@
+// const cars = ["A", "B", "c", "d", "e"];
+
+// for (let i = 0; i < cars.length; i++) {
+//     console.log(i);
+// }
+
+// 1. Print all even numbers from 0 – 10
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
